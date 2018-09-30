cf16c233e21abde78558530627ef038c9b0c20a4
Frederik Skytte <frederik@skyttenielsen.dk>
2018-09-30 20:27:24 +0200

    Changes to main

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char* argv[])
 {
-    printf("Det skal da mindst være et hello world jesper :)\n");
+    if(argc < 3)
+    {
+        printf("Boo!\n");
+        return -1;
+    }
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    printf("%d + %d = %d\n", a, b, a + b);
 }
