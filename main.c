046155f3b282bb5f48eeaec36dbda78f46e66196
Frederik Skytte <frederik@skyttenielsen.dk>
2018-09-30 20:34:35 +0200

    New and improved main :P

--- main.c
+++ main.c
@@ -2,12 +2,5 @@
 
 int main(int argc, char* argv[])
 {
-    if(argc < 3)
-    {
-        printf("Boo!\n");
-        return -1;
-    }
-    int a = atoi(argv[1]);
-    int b = atoi(argv[2]);
-    printf("%d + %d = %d\n", a, b, a + b);
+    printf("Det her er en meget bedre funktion end før\n");
 }
